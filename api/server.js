82fd602774416b485af4876208d8adcdfeea728e
Alexander Aymond <lynxkhaos@gmail.com>
2022-07-18 18:41:41 -0500

    completed

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 
+const actRouter = require('./accounts/accounts-router')
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', actRouter);
+
+server.use('*', (req, res) => {
+    res.status(404).json({message: "Not found"})
+})
 
 module.exports = server;
